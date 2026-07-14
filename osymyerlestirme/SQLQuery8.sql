b44a8ea05a8bc9f94ba7ebfec3d7ecab871feefc
edaaytas_ <128994656+Edaaytas@users.noreply.github.com>
2026-07-14 11:53:30 +0300

    Delete osymyerlestirme/SQLQuery8.sql

--- osymyerlestirme/SQLQuery8.sql
+++
@@ -1,4 +0,0 @@
-BACKUP DATABASE [OSYMYerlestirme2] TO  DISK = N'C:\Program Files\
-Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Backup\fullosym.bak' 
-WITH NOFORMAT, NOINIT,  NAME = N'OSYMYerlestirme2-Full Database Backup', SKIP, NOREWIND, NOUNLOAD,  STATS = 10
-GO
